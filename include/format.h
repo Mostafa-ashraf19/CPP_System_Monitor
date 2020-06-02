abb947f5e5d3159dc2b9a38cc8d47659a64a7f1e
mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2020-06-02 23:40:53 +0200

    Change time formating

---
+++ include/format.h
@@ -0,0 +1,11 @@
+#ifndef FORMAT_H
+#define FORMAT_H
+
+#include <string>
+#include "linux_parser.h"
+
+namespace Format {
+std::string ElapsedTime(long times);  
+};                                    
+
+#endif
