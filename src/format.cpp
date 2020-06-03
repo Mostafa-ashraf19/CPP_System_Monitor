b5a9df704d5506b35eead69f6a31ab92a982f5a2
mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2020-06-03 21:28:54 +0200

    Change time formating

--- src/format.cpp
+++ src/format.cpp
@@ -3,8 +3,9 @@
 #include "format.h"
 
 using std::string;
+using std::to_string;
 
-string Format::ElapsedTime(long seconds) {
+/*string Format::ElapsedTime(long seconds) {
 
     float AllHours = static_cast<float>(seconds)/static_cast<float>(3600);
    
@@ -13,4 +14,17 @@ string Format::ElapsedTime(long seconds) {
     return std::to_string(static_cast<int>(AllHours))+':'+
             std::to_string(static_cast<int>(Allmin))+':'+
             std::to_string(static_cast<int>(Allsec));    
- }
+ }*/
+
+ std::string Format::Pad(std::string s, char c) {
+  s.insert(s.begin(), 2 - s.size(), c);
+  return s;
+}
+string Format::ElapsedTime(long  time) {
+  int hours = time / (60 * 60);
+  int minutes = (time / 60) % 60;
+  long seconds = time % 60;
+  return std::string(Format::Pad(to_string(hours), '0') + ":" +
+                     Format::Pad(to_string(minutes), '0') + ":" +
+                     Format::Pad(to_string(seconds), '0'));
+}
